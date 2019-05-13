0972f8e7c2150a4046145faa1ad42f42879b4c14
israel gomes <rael_goomes@hotmail.com>
2019-05-13 19:08:51 -0300

    Angular aplication

---
+++ out-tsc/app/app/app.route.js
@@ -0,0 +1,7 @@
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+export var ROUTES = [
+    { path: '', component: HomeComponent },
+    { path: 'about', component: AboutComponent }
+];
+//# sourceMappingURL=app.route.js.map
